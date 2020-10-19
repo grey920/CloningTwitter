1c06ec0160fff409ecd721e22e763f6599613fdb
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-10-19 21:40:12 +0900

    리덕스 적용

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+const store = createStore(rootReducer, composeWithDevTools()); // 상태를 다루고 저장하는 store 생성
+
+// Provider를 통해 리액트 프로젝트에 리덕스를 적용한다
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
