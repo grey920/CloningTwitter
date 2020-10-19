d40c5eabf8b5cb2f9e88efe5e2d5c34e0828189a
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-10-19 21:20:08 +0900

    main페이지에 Button 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import UserList from './pages/UserList';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route component={MainPage} exact path="/" />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={UserList} path="/userList" />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/pages/UserList.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const UserList = () => {
+    return <div>회원 목록</div>;
+}
+export default UserList;
